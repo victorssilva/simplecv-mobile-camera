b0f39f6dce67dbac49f0c0adad00d737b7e0f9ff
Victor Silva <victsou@gmail.com>
2012-06-21 03:26:24 -0300

    Addition of Progress Dialog
    App was hanging during the upload. As mobile connections tends to be slower,
    we have to show the user the application is still running.
    
    Fixes #10

--- simplecv-android-app/src/com/simplecv/hellocamera/HelloCameraNewApiActivity.java
+++ simplecv-android-app/src/com/simplecv/hellocamera/HelloCameraNewApiActivity.java
@@ -26,11 +26,14 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -43,36 +46,35 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class HelloCameraNewApiActivity extends Activity {
-	
+
 	private static final int TAKE_PICTURE = 0;
 	private static final int SELECT_PICTURE = 1;
 	private boolean pictureIsSet = false;
-	
+
 	private static String uploadURL = "http://10.0.2.2:8000/upload";
 	private static String modifyURL = "http://10.0.2.2:8000/process";
-	
-	protected ImageView capturedImage;
-	protected ImageView transformedImage;
-	protected Uri pictureUri;
-	protected String pathToPicture;
-	
+
+	private ImageView capturedImage;
+	private Uri pictureUri;
+	private String pathToPicture;
+
 	private String transformation = null;
 	private String linkToOriginal = null;
-	
+
 	/* Possible transformations on spinner */
 	private static final int INVERT = 0;
 	private static final int GRAB_EDGES = 1;
 	private static final int DIVIDE = 2;
 	private static final int DILATE = 3;
 
-	
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         capturedImage = (ImageView) findViewById(R.id.capturedimage);
-        
+
         Spinner spinner = (Spinner) findViewById(R.id.transformations_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.transformations_array, android.R.layout.simple_spinner_item);
@@ -80,7 +82,7 @@ public class HelloCameraNewApiActivity extends Activity {
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new onTransformationSelectedListener());
     }
-   
+
     public File getNewPictureFile(){
 		File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "SimpleCV");
 		if (!mediaStorageDir.exists()){
@@ -89,12 +91,12 @@ public class HelloCameraNewApiActivity extends Activity {
 		    	mediaStorageDir = Environment.getExternalStorageDirectory();
 	        }
 	    }
-		
+
         String timeStamp = new SimpleDateFormat("MMdd_HHmmss").format(new Date());
-        File file = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");		
+        File file = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
     	return file;
     }
-    
+
     public Uri getUriFromBitmap(Bitmap bitmap) {
 		File newPictureFile = getNewPictureFile();
 		try {
@@ -108,8 +110,8 @@ public class HelloCameraNewApiActivity extends Activity {
 		}
         return Uri.fromFile(newPictureFile);
     }
-   
-   
+
+
 	public void takePicture(View view){
 
     	Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -123,63 +125,103 @@ public class HelloCameraNewApiActivity extends Activity {
     	intent.setType("image/*");
     	startActivityForResult(Intent.createChooser(intent,"Select an image"),SELECT_PICTURE);
     }
-    
-    
+
+
     public String getPathFromGallery(Uri uri) {
-    	Cursor cursor = getContentResolver().query(uri, null, null, null, null); 
-        cursor.moveToFirst(); 
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA); 
-        return cursor.getString(idx); 
+    	Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
     }
     
     public void processPicture(View view) {
-    	if (pictureIsSet == true && transformation != null) {
+		if (pictureIsSet == true && transformation != null) {
+			new UploadImageTask(this).execute();
+		}	
+    }
+    
+    private class UploadImageTask extends AsyncTask<Void, String, Void> {
+    	
+    	private Context context;
+    	ProgressDialog progressDialog;
+		    
+    	public UploadImageTask(Context cxt) {
+            context = cxt;
+            progressDialog = new ProgressDialog(context);
+        }
+    	
+    	
+    	protected void onPreExecute() {
+    		progressDialog.setMessage("Preparing picture...");
+    		progressDialog.show();
+        }	
+    	
+        protected Void doInBackground(Void... unused) {
+        	
     		if (linkToOriginal == null) {
-    			uploadPicture(view);
+	        	this.publishProgress("Uploading picture...");
+    			uploadPicture();
     		}
     		
-			modifyPicture(view);
-    	}
+        	this.publishProgress("Getting picture back...");
+			modifyPicture();
+    			
+			return (null);
+        }
+        
+        protected void onPostExecute(Void unused) {
+        	progressDialog.dismiss();
+        }
+        
+        protected void onProgressUpdate(String... message) {
+    		progressDialog.setMessage(message[0]);
+    		progressDialog.show();
+        }
     }
     
-    public void uploadPicture(View view) {
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpPost httpPost = new HttpPost(uploadURL);
-		httpPost.setHeader("User-Agent", "SimpleCV Mobile Camera");
+    public void uploadPicture() {
+
 		
-		try {
-			MultipartEntity entity = new MultipartEntity();
-		 
-			entity.addPart("type", new StringBody("file"));
-			entity.addPart("data", new FileBody(new File(pathToPicture),"image/jpeg"));
-			httpPost.setEntity(entity);
-		  
-			HttpResponse httpResponse = httpclient.execute(httpPost);
-		  
-			HttpEntity responseEntity = httpResponse.getEntity(); 
-			if(responseEntity!=null) {
-				linkToOriginal = EntityUtils.toString(responseEntity);
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httpPost = new HttpPost(uploadURL);
+			httpPost.setHeader("User-Agent", "SimpleCV Mobile Camera");
+
+			try {
+				MultipartEntity entity = new MultipartEntity();
+
+				entity.addPart("type", new StringBody("file"));
+				entity.addPart("data", new FileBody(new File(pathToPicture),"image/jpeg"));
+				httpPost.setEntity(entity);
+
+				HttpResponse httpResponse = httpclient.execute(httpPost);
+
+				HttpEntity responseEntity = httpResponse.getEntity();
+				if(responseEntity!=null) {
+					linkToOriginal = EntityUtils.toString(responseEntity);
+				}
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
     }
-    
-    public void modifyPicture(View view) {
+
+
+    public void modifyPicture() {
 		HttpClient httpclient = new DefaultHttpClient();
 		HttpPost httpPost = new HttpPost(modifyURL);
 		httpPost.setHeader("User-Agent", "SimpleCV Mobile Camera");
-		
-		try { 
+
+		try {
 			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
 	        nameValuePairs.add(new BasicNameValuePair("picture", linkToOriginal));
 	        nameValuePairs.add(new BasicNameValuePair("transformation", transformation));
 	        httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
 			HttpResponse httpResponse = httpclient.execute(httpPost);
-		  
-			HttpEntity responseEntity = httpResponse.getEntity(); 
+
+			HttpEntity responseEntity = httpResponse.getEntity();
 			if(responseEntity!=null) {
 				String transformedImageURL = EntityUtils.toString(responseEntity);
 				try {
@@ -187,6 +229,8 @@ public class HelloCameraNewApiActivity extends Activity {
 					Uri transformedImageUri = getUriFromBitmap(transformedImageBitmap);
 					Intent displayIntent = new Intent(getApplicationContext(), DisplayResultsActivity.class);
 					displayIntent.putExtra("uriAsString", transformedImageUri.toString());
+					
+					
 					startActivity(displayIntent);
 		    	} catch (MalformedURLException e) {
 		    	  e.printStackTrace();
@@ -200,8 +244,8 @@ public class HelloCameraNewApiActivity extends Activity {
 			e.printStackTrace();
 		}
   }
-    
-    
+
+
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode == RESULT_OK)
 		{
@@ -213,13 +257,13 @@ public class HelloCameraNewApiActivity extends Activity {
 					pictureUri = data.getData();
 					pathToPicture = getPathFromGallery(pictureUri);
 					break;
-			}	
+			}
 			capturedImage.setImageURI(pictureUri);
 			pictureIsSet = true;
 			linkToOriginal = null;
 		}
 	}
-	
+
 	public class onTransformationSelectedListener implements OnItemSelectedListener {
 
 	    public void onItemSelected(AdapterView<?> parent,
@@ -229,7 +273,7 @@ public class HelloCameraNewApiActivity extends Activity {
 	    			transformation = "invert";
 	    		break;
 	    		case GRAB_EDGES:
-	    			transformation = "edges";	    		
+	    			transformation = "edges";
 	    		break;
 	    		case DIVIDE:
 	    			transformation = "divide";
@@ -241,8 +285,8 @@ public class HelloCameraNewApiActivity extends Activity {
 	    }
 
 	    public void onNothingSelected(AdapterView<?> parent) {
-	    	
+
 	    }
 	}
-	
+
 }
